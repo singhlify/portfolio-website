4c4232c9c385d0ed5401dd9bdc9348725f0a4213
Blink98 <gs130899@gmail.com>
2022-03-15 23:23:05 +0530

    :hammer_and_wrench: [UPDATE] use mounting

--- styles/theme.js
+++ styles/theme.js
@@ -1,5 +1,6 @@
 // https://brianlovin.com/writing/adding-dark-mode-with-next-js
 // https://codesandbox.io/s/mzj64x80ny?file=/src/styles.scss
+// https://www.joshwcomeau.com/react/dark-mode/
 
 import { useEffect, useState } from "react";
 import useDarkMode from "use-dark-mode";
@@ -10,45 +11,25 @@ import GlobalStyles from "./globals";
 const Theme = ({ children }) => {
 	const { value } = useDarkMode(true);
 	const theme = value ? darkTheme : lightTheme;
+	const [mounted, setMounted] = useState(false);
 
-	// useEffect(() => {
-	// 	console.log("value>>>", value);
-	// }, [value]);
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
-	// const [mounted, setMounted] = useState(false);
-
-	// useEffect(() => {
-	// 	let cancel = true;
-
-	// 	if (cancel) {
-	// 		setMounted(true);
-	// 	}
-
-	// 	return () => {
-	// 		cancel = false;
-	// 	};
-	// }, []);
-
-	// const body = (
-	// 	<ThemeProvider theme={theme}>
-	// 		<GlobalStyles />
-	// 		{children}
-	// 	</ThemeProvider>
-	// );
-
-	// prevents ssr flash for mismatched dark mode
-	// if (!mounted) {
-	// 	return <div style={{ visibility: "hidden" }}>{body}</div>;
-	// }
-
-	// return body;
-
-	return (
+	const body = (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
 			{children}
 		</ThemeProvider>
 	);
+
+	// prevents ssr flash for mismatched dark mode
+	if (!mounted) {
+		return <div style={{ visibility: "hidden" }}>{body}</div>;
+	}
+
+	return body;
 };
 
 export default Theme;
