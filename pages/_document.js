343dea19fd2f0a0691f6d5e021eb39fedee7113a
Blink98 <gs130899@gmail.com>
2022-04-12 22:46:09 +0530

    :hammer_and_wrench: [UPDATE] import font-awesome in _document.js from Home.js

--- pages/_document.js
+++ pages/_document.js
@@ -42,6 +42,10 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&display=swap"
 						rel="stylesheet"
 					/>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+				/>
 				</Head>
 				<body>
 					<Main />
