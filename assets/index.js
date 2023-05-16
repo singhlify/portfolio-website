73fceaabe42a1f1a0a706b02502fca28320f210f
Gurjot Singh <gs130899@gmail.com>
2023-05-16 12:19:39 +0530

    fix: use env for logo path

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,2 @@
 export { default as devWorking } from "./images/dev_working.png";
+export { default as logo } from "./images/logo.png";
--- components/MicroComponents/MetaTags.js
+++ components/MicroComponents/MetaTags.js
@@ -1,32 +1,38 @@
 import Head from "next/head";
 
 const MetaTags = ({ title, description, img, children = [] }) => {
-	return (
-		<Head>
-			{/* HTML Meta Tags */}
-			<title>{title}</title>
-			<meta name="description" content={description} />
+  let defaultImg = img;
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    defaultImg = `${process.env.NEXT_PUBLIC_BASE_URL}${img}`;
+  }
 
-			{children.length > 0 && children}
+  return (
+    <Head>
+      {/* HTML Meta Tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
 
-			{/* Google / Search Engine Tags */}
-			<meta itemProp="name" content={title} />
-			<meta itemProp="description" content={description} />
-			<meta itemProp="image" content={img} />
+      {children.length > 0 && children}
 
-			{/* Facebook Meta Tags */}
-			<meta property="og:type" content="website" />
-			<meta property="og:title" content={title} />
-			<meta property="og:description" content={description} />
-			<meta property="og:image" content={img} />
+      {/* Google / Search Engine Tags */}
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={description} />
+      <meta itemProp="image" content={defaultImg} />
 
-			{/* Twitter Meta Tags */}
-			<meta name="twitter:card" content="summary_large_image" />
-			<meta name="twitter:title" content={title} />
-			<meta name="twitter:description" content={description} />
-			<meta name="twitter:image" content={img} />
-		</Head>
-	);
+      {/* Facebook Meta Tags */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={defaultImg} />
+      <meta property="og:url" content={`${process.env.NEXT_PUBLIC_BASE_URL}`} />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={defaultImg} />
+    </Head>
+  );
 };
 
 export default MetaTags;
